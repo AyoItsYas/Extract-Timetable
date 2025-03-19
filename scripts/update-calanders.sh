e9362451f3c3648078ef276f967edf9244f4b525
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2025-03-19 15:43:03 +0530

    feat: faster extraction script

--- scripts/update-calanders.sh
+++ scripts/update-calanders.sh
@@ -10,6 +10,21 @@ file=$1
 mkdir -p ./.tmp
 mkdir -p ./calanders
 
+i=1
+while read -r line; do
+  if [[ $line =~ [^/]*$ ]]; then
+    ANCHOR=$(echo "$line" | cut -d' ' -f1)
+    HREF=$(echo "$line" | cut -d' ' -f2)
+    FILENAME=$(echo "$HREF" | rev | cut -d'/' -f1 | rev)
+  fi
+
+  echo "Downloading sheet @ '$HREF' ..."
+  wget --cookies=on --keep-session-cookies "$HREF&download=1" -O "./.tmp/$FILENAME.xlsx" --quiet &
+
+done < "$file"
+
+wait
+echo "All downloads completed."
 
 i=1
 while read line; do
@@ -19,10 +34,10 @@ while read line; do
     FILENAME=$(echo $HREF | rev | cut -d'/' -f1 | rev)
   fi
 
-  echo "Downloading sheet @ '$HREF' ..."
-  wget --cookies=on --keep-session-cookies "$HREF&download=1" -O "./.tmp/$FILENAME.xlsx" --quiet
-
-  python3 main.py "./.tmp/$FILENAME.xlsx" --output "%SUMMARY% - %WS_TITLE% -- ($FILENAME).ics" --output_folder "./calanders/" --anchor "$ANCHOR"
+  python3 main.py "./.tmp/$FILENAME.xlsx" --output "%SUMMARY% - %WS_TITLE% -- ($FILENAME).ics" --output_folder "./calanders/" --anchor "$ANCHOR" &
 
   i=$((i+1))
 done < "$file"
+
+wait
+echo "All conversions completed."
